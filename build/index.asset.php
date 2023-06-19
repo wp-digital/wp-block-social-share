2ebd900bb8808312a860774cdf4964cc3f3cdee2
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2023-06-19 23:36:22 +0300

    rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '05831e66b1cecc175b37ffb42ad3d649');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '022a570667e567c9f79e');
