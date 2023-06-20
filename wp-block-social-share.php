419ecb8b0d20c1c0cf98f838d4d22ee079917c13
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2023-06-20 15:46:53 +0300

    fix package name

--- wp-block-social-share.php
+++ wp-block-social-share.php
@@ -4,7 +4,7 @@
  * Description:       Links to share current post to most used social such as Facebook, Twitter, Email, LinkedIn.
  * Requires at least: 5.8
  * Requires PHP:      7.1
- * Version:           2.4.0
+ * Version:           2.5.0
  * Author:            SMFB Dinamo
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
